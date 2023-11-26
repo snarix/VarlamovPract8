c18eedaf27905386534c199c2478e02e0cf4a53a
snarix <sanr@mail.ru>
2023-11-26 18:21:18 +0300

    Добавьте файлы проекта.

---
+++ VarlamovPract8/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace VarlamovPract8
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ VarlamovPract8/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace VarlamovPract8
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadPhoto(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void stepperAvgRate_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            Rate.Text = stepperAvgRate.Value.ToString();
+            Rate.Text = e.NewValue.ToString();
+        }
+    }
+}
